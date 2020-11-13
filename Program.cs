75b480e19c07ed22abb4d016262c3d299dc44665
DEV <dev@info.local>
2020-11-13 08:15:10 -0300

    media

--- Program.cs
+++ Program.cs
@@ -23,15 +23,33 @@ namespace Senai_backend
 
             media = (nota1 + nota2 + nota3)/3;
 
+            while (nota1 > 10 || nota2 > 10 || nota3 > 10)
+            {
+                if (nota1 > 10)
+                {
+                    Console.Write("Nota inválida, digite a nota 1 novamente: ");
+                    nota1 = float.Parse(Console.ReadLine());
+                }
+                if (nota2 > 10)
+                {
+                    Console.Write("Nota inválida, digite a nota 2 novamente: ");
+                    nota2 = float.Parse(Console.ReadLine());
+                }
+                if (nota3 > 10)
+                {
+                    Console.Write("Nota inválida, digite a nota 3 novamente: ");
+                    nota3 = float.Parse(Console.ReadLine());
+                }
+            }
             if (media <= 5)
             {
                 Console.WriteLine("O aluno "+nome+" não passou de ano");
-                Console.WriteLine(media);
+                Console.WriteLine("Média: "+media);
             }
             else
             {
                 Console.WriteLine("O alnuno "+nome+" passou de ano, parabens!");   
-                Console.WriteLine(media);
+                Console.WriteLine("Média: "+media);
             }
         }
     }
